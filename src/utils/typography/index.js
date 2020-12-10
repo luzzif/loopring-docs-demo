fda007852d3efb0b98a5f5e7cf5242861835c5b0
Federico Luzzi <fedeluzzi00@gmail.com>
2020-12-10 23:50:17 +0100

    feat: use inter font for bodies

--- src/utils/typography/index.js
+++ src/utils/typography/index.js
@@ -2,10 +2,10 @@ import Typography from "typography";
 
 const typography = new Typography({
     baseFontSize: "16px",
-    baseLineHeight: "22px",
+    baseLineHeight: "24px",
     headerFontFamily: ["Montserrat", "sans-serif"],
     headerWeight: "700",
-    bodyFontFamily: ["Montserrat", "sans-serif"],
+    bodyFontFamily: ["Inter", "sans-serif"],
     bodyWeight: "400",
 });
 
